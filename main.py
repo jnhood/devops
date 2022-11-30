990bf3c84e1ac73134321a12d53343ceada8d25d
JaKia Hood <112641697+jnhood@users.noreply.github.com>
2022-11-30 18:25:10 +0000

    updated main

--- main.py
+++ main.py
@@ -4,13 +4,6 @@ def square(n):
     return n*n
   
 def cube(n):
-  return n*n*n
+    return n*n*n
 
-def test_square():
-  assert square(4)==16
-  assert square(-2)==4
-
-def test_cube():
-  assert cube(5)==125
-  assert cube(-2)==-8
 
